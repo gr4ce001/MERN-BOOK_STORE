595fd8e992e3ec28cceef1980843a33613ad58ea
GR4CE <gr4ce001@gmail.com>
2023-11-14 14:38:56 +0530

    added existing book updation route by put method

--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -8,33 +8,65 @@ router.get("/", (req, res) => {
   return res.status(234).send("WElcome to mern stack project");
 });
 
-router.route("/books").post(async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({
-        message: `send all required fields`,
-      });
+router
+  .route("/books")
+  .post(async (req, res) => {
+    try {
+      if (!req.body.title || !req.body.author || !req.body.publishYear) {
+        return res.status(400).send({
+          message: `send all required fields`
+        });
+      }
+      const newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        publishYear: req.body.publishYear,
+      };
+
+      const book = await Book.create(newBook);
+      return res.status(201).send(book);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send({ message: error.message });
     }
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
-
-    const book = await Book.create(newBook);
-    return res.status(201).send(book);
-
+  })
+  .get(async (req, res) => {
+    try {
+      const books = await Book.find({});
+      return res.status(200).json({ count: books.length, data: books });
+    } catch (error) {}
+  });
+router.get("/books/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
   }
-}).get(async (req,res)=>{
-    try {
-        const books=await Book.find({})
-        return res.status(200).json(books);
-    } catch (error) {
-        
+});
+
+router.put("/books/:id", async (req, res) => {
+  try {
+    if(!req.body.title || !req.body.author||!req.body.publishYear)
+    {
+      return res.status(400).send({
+        message: `send all required fields`})
     }
-})
+
+    const id = req.params.id;
+    const result = await Book.findByIdAndUpdate(id,req.body);
+    if(!result){
+      return res.status(404).json({message :'book not found'})
+    }
+    return res.status(200).send({message:'book updated successfully'});
+
+    }
+   catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }}
+);
 
 export default router;
